e7feac16d047b2f83707befdd1bf4af836077a0d
DevadharshiniSK <devashanmugaraj43@gmail.com>
2022-10-09 21:48:37 +0530

    1st commit

---
+++ string.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='('){
+                count+=1;
+            }
+            else if(str.charAt(i)==')'){
+                count-=1;
+            }
+        }
+        if(count==0){
+            System.out.println("0");
+        }
+        else{
+           System.out.println("1"); 
+        }
+    }
+}
